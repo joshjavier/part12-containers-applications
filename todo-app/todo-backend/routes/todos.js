42bb0b4fdb177c73b21bf55ebd3049d7436e6a0a
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-09-08 11:59:57 +0800

    Exercise 12.7: Little bit of MongoDB coding

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -29,18 +29,29 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body
+
+  if (text)
+    req.todo.text = text
+  if (done)
+    req.todo.done = done
+
+  const savedTodo = await req.todo.save()
+  res.status(200).send({
+    message: `Updated todo ${savedTodo.id}`,
+    todo: savedTodo,
+  })
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
